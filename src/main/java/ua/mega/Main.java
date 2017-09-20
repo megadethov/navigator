88e3c8b1b8abe39eae36c8f44af0ed98719da8cf
Yuriy Yefremov <megadethov@gmail.com>
2017-09-20 21:48:29 +0300

    add logging

---
+++ src/main/java/ua/mega/Main.java
@@ -0,0 +1,11 @@
+package ua.mega;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger LOG = LoggerFactory.getLogger(Main.class);
+        LOG.info("Test logging");
+    }
+}
