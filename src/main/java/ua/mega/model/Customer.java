2ccdaba0477e4782b54a4ec853e77258684b9d1b
Yuriy Yefremov <megadethov@gmail.com>
2017-09-25 12:32:14 +0300

    switch to GenerationType.IDENTITY - resolve ibernate could not get next sequence value

--- src/main/java/ua/mega/model/Customer.java
+++ src/main/java/ua/mega/model/Customer.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String patronymic;
--- src/main/java/ua/mega/model/Phone.java
+++ src/main/java/ua/mega/model/Phone.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 public class Phone {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(unique = true, nullable = false)
